a5f09dca8cb734f93d7b046335e8c5359cad6c1b
uiux@okdollar frontend <uiux@okdollar.com>
2024-12-04 13:27:39 +0630

    change password

--- src/app/component/change-password/change-password.component.ts
+++ src/app/component/change-password/change-password.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-change-password',
@@ -7,12 +8,36 @@ import { Router } from '@angular/router';
   styleUrls: ['./change-password.component.css']
 })
 export class ChangePasswordComponent implements OnInit {
-constructor(private router:Router){}
+constructor(private router:Router,private service:ApiService){}
+
+confirmPassword:string = ''
+newPassword:string = ''
+postDetails = {
+  password:''
+}
 ngOnInit(): void {
     
 }
   cancel(){
     this.router.navigateByUrl('customer/profile')
+  }
+
+  changePassword(){
+    const userId = localStorage.getItem('userId')
+    if(this.newPassword === this.confirmPassword){
+      this.postDetails = {
+        password:this.confirmPassword
+      }
+      this.service.patchData(`users/${userId}`,this.postDetails).subscribe((res:any)=>{
+        if(res){
+          this.router.navigateByUrl('customer/profile')
+        }
+      },error =>{
+        console.log(error, 'change password error is')
+      })
+    }else{
+      throw Error('Password does not match')
+    }
   }
 
 }
